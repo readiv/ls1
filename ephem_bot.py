db6673dc3ae4eabf2339449f6308da711e558739
Mikhail Volkov <hd3200@gmail.com>
2019-09-13 21:18:16 +0200

    theEnd

--- ephem_bot.py
+++ ephem_bot.py
@@ -12,7 +12,7 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
+import logging, ephem, datetime, my_settings
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -22,32 +22,57 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 )
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
-
-
 def greet_user(bot, update):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    if update == None or update.message == None:
+        return #Периоди́чески возникает такая хрень    
+    update.message.reply_text('Привет! Я ephem bot')
+    update.message.reply_text('Хотите узнать, в каком созвездии находится Ваша планета сегодня? Введите /planet название планеты на английском')
 
+def planet(bot, update):
+    edata = datetime.date.today()
+    edata = str(edata.year)+'/'+str(edata.month)+'/'+str(edata.day)
+    ephem_dic = {'mars':ephem.Mars(edata),
+                 'moon':ephem.Moon(edata),
+                 'mercury':ephem.Mercury(edata),
+                 'venus':ephem.Venus(edata),
+                 'jupiter':ephem.Jupiter(edata),
+                 'saturn':ephem.Saturn(edata),
+                 'uranus':ephem.Uranus(edata),
+                 'neptune':ephem.Neptune(edata),
+                 'pluto':ephem.Pluto(edata),
+                 }
+    try:
+        if update == None or update.message == None:
+            return #Периоди́чески возникает такая хрень
+        text = update.message.text.split(' ')
+        if len(text)<2:
+            raise ValueError('Ошибка. Вы не ввели название планеты')
+        text = text[1].lower() #Получили название планеты
+        ephem_one = ephem_dic.get(text) 
+        if ephem_one==None:
+            raise ValueError('Ошибка. Я не знаю такой планеты')
+        const = ephem.constellation(ephem_one)
+        if len(const) != 2: #На всякий случай
+            raise ValueError('Ошибка. Я не знаю такой планеты')
+            
+        update.message.reply_text(f'Планета {text.upper()} сегодня находится в созвездии "{const[1].upper()}"')  
+    except ValueError as e:
+        update.message.reply_text(str(e))
+        update.message.reply_text('Вы должны набрать /planet планета')
+        update.message.reply_text('Сейчас я знаю такие планеты: '+str(list(ephem_dic))[1:-1])
 
-def talk_to_me(bot, update):
+def talk_to_me(bot, update): 
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
  
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
+    mybot = Updater(my_settings.token_telegram)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     mybot.start_polling()
